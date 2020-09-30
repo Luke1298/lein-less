d21c4f5ea6fea0386687b146fe0988818d442489
Luke Nelson <luke@viderahealth.com>
2020-09-30 11:38:22 -0600

    Allow directory to be watched without being complied.
    Change project name.

--- project.clj
+++ project.clj
@@ -1,15 +1,14 @@
-(defproject lein-less "1.7.6-SNAPSHOT"
-  :description "Less CSS compiler plugin for leiningen"
-  :url "http://github.com/montoux/lein-less"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject org.clojars.lukenelson/lein-less "1.7.6-SNAPSHOT"
+            :description "Less CSS compiler plugin for leiningen"
+            :url "http://github.com/Luke1298/lein-less"
+            :license {:name "Eclipse Public License"
+                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :java-source-paths ["java"]
-  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+            :java-source-paths ["java"]
+            :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
 
-  :eval-in :leiningen
-  :min-lein-version "2.3.0"
+            :eval-in :leiningen
+            :min-lein-version "2.3.0"
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0" :optional true]
-                                  [leiningen-core "2.5.1" :optional true]]}}
-  )
+            :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0" :optional true]
+                                            [leiningen-core "2.5.1" :optional true]]}})
--- src/leiningen/less.clj
+++ src/leiningen/less.clj
@@ -2,13 +2,12 @@
   (:require (leiningen compile help)
             (leiningen.core.main)
             (leiningen.less
-              [compiler :as compiler]
-              [config :refer [config]]
-              [nio :as nio])
+             [compiler :as compiler]
+             [config :refer [config]]
+             [nio :as nio])
             [robert.hooke :as hooke]
             [leiningen.less.engine :as engine])
   (:import (leiningen.less LessError)))
-
 
 (defn- report-error [^LessError error]
   (binding [*out* *err*]
@@ -19,17 +18,17 @@
 
 (defn- run-compiler
   "Run the lesscss compiler."
-  [project {:keys [source-paths target-path] :as config} watch?]
+  [project {:keys [source-paths watch-paths target-path] :as config} watch?]
   (engine/with-engine "javascript"
-    (compiler/initialise)
-    (println "Compiling {less} css:")
-    (let [units (nio/compilation-units source-paths target-path)
-          on-error (if watch? report-error abort-on-error)
-          compile (partial compiler/compile-project project units on-error)]
-      (if watch?
-        (nio/watch-resources project source-paths compile)
-        (compile))
-      (println "Done."))))
+                      (compiler/initialise)
+                      (println "Compiling {less} css:")
+                      (let [units (nio/compilation-units source-paths target-path)
+                            on-error (if watch? report-error abort-on-error)
+                            compile (partial compiler/compile-project project units on-error)]
+                        (if watch?
+                          (nio/watch-resources project watch-paths compile)
+                          (compile))
+                        (println "Done."))))
 
 (defn- once
   "Compile less files once."
@@ -52,9 +51,7 @@
    (let [config (config project)]
      (case subtask
        "once" (apply once project config args)
-       "auto" (apply auto project config args)
-       )))
-  )
+       "auto" (apply auto project config args)))))
 
 (defn compile-hook [task & args]
   (apply task args)
